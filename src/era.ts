b718310726624e72a97f6f3a9cab52bf9a4e9c79
Undercrow <undercrow@protonmail.com>
2021-10-08 03:55:31 +0900

    Load zip file contents in parallel

--- src/era.ts
+++ src/era.ts
@@ -1,11 +1,11 @@
 import * as era from "erajs";
-import JSZip, {JSZipObject} from "jszip";
+import JSZip from "jszip";
 
 export async function compile(zip: JSZip): Promise<era.VM> {
 	const erh: string[] = [];
 	const erb: string[] = [];
 	const data = new Map<string, string>();
-	for (const zipObj of Object.values(zip.files)) {
+	await Promise.all(Object.values(zip.files).map(async (zipObj) => {
 		const name = zipObj.name;
 		if (name.startsWith("ERB/") && (name.endsWith(".erh") || name.endsWith(".ERH"))) {
 			erh.push(await zipObj.async("text"));
@@ -15,29 +15,29 @@ export async function compile(zip: JSZip): Promise<era.VM> {
 			const baseName = name.split("/").pop()!;
 			data.set(baseName, await zipObj.async("text"));
 		}
-	}
+	}));
 
 	return era.compile(erh, erb, data);
 }
 
 export async function hash(zip: JSZip): Promise<string> {
-	const files = new Map<string, JSZipObject>();
-	for (const zipObj of Object.values(zip.files)) {
+	const files = new Map<string, string>();
+	await Promise.all(Object.values(zip.files).map(async (zipObj) => {
 		const name = zipObj.name;
 		if (
 			(name.startsWith("ERB/") && (name.endsWith(".erh") || name.endsWith(".ERH"))) ||
 			(name.startsWith("ERB/") && (name.endsWith(".erb") || name.endsWith(".ERB"))) ||
 			(name.startsWith("CSV/") && (name.endsWith(".csv") || name.endsWith(".CSV")))
 		) {
-			files.set(name, zipObj);
+			files.set(name, await zipObj.async("text"));
 		}
-	}
+	}));
 
 	const keys = [...files.keys()];
 	keys.sort();
 	let content = "";
 	for (const key of keys) {
-		content += `####${key}####` + "\n" + await files.get(key)!.async("text") + "\n";
+		content += `####${key}####` + "\n" + files.get(key)! + "\n";
 	}
 	const digest = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(content));
 	const arr = Array.from(new Uint8Array(digest));
