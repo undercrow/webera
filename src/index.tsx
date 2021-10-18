4a5322623e22e50811f34bb99a1190ff6c85d4eb
Undercrow <undercrow@protonmail.com>
2021-10-18 22:27:04 +0900

    Use HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import {h} from "preact";
 
 import {render} from "preact";
 import {Provider as ReduxProvider} from "react-redux";
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 
 import "./reset.css";
 
@@ -11,7 +11,7 @@ import App from "./App";
 
 render((
 	<ReduxProvider store={store}>
-		<Router basename={BASENAME}>
+		<Router basename={BASENAME} hashType="noslash">
 			<App />
 		</Router>
 	</ReduxProvider>
